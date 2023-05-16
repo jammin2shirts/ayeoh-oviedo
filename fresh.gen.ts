273220d846212661b57b4ce10a8f5a85b2074fec
jammin2shirts <teuchertb@gmail.com>
2023-05-16 13:34:28 -0400

    commit sitemap

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,12 +5,14 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/api/mailer.tsx";
 import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/sitemap.xml.ts";
 import * as $$0 from "./islands/Contact-Form.tsx";
 
 const manifest = {
   routes: {
     "./routes/api/mailer.tsx": $0,
     "./routes/index.tsx": $1,
+    "./routes/sitemap.xml.ts": $2,
   },
   islands: {
     "./islands/Contact-Form.tsx": $$0,
