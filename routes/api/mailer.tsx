6807cc5d9bb0a715c15891b92a28ab4045243272
jammin2shirts <teuchertb@gmail.com>
2023-05-15 22:55:39 -0400

    Updated

--- routes/api/mailer.tsx
+++ routes/api/mailer.tsx
@@ -1,87 +1,56 @@
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
-import { load } from "https://deno.land/std@0.187.0/dotenv/mod.ts";
 import { Handlers } from "$fresh/server.ts";
-import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
-
-
-
-
-
-
+import { SMTPClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts";
 
 export const handler: Handlers = {
   async POST(req: Request) {
+    const headers = new Headers(req.headers);
+    headers.set("location", "/");
+
     const form = await req.formData();
-    const fullName = form.get('fullName')?.toString()
-    const email = form.get('email')?.toString()
-    const message = form.get('message')?.toString()
+    const fullName = form.get("fullName")?.toString();
+    const email = form.get("email")?.toString();
+    const message = form.get("message")?.toString();
     if (!fullName || !email || !message) {
-        console.warn(`this didn't work`)
-        return new Response();
+      console.warn(`form data didn't work`);
+      return new Response(null, {
+        status: 302,
+        headers,
+      });
     }
-    console.debug('made it here')
 
     const username = Deno.env.get("USERNAME");
-    const password =  Deno.env.get("PASSWORD");
+    const password = Deno.env.get("PASSWORD");
     if (!username || !password) {
-        console.warn(`env didn't work`)
-        return new Response();
+      console.warn(`env didn't work`);
+      return new Response(null, {
+        status: 302,
+        headers,
+      });
     }
     const client = new SMTPClient({
-        connection: {
-          hostname: "email-smtp.us-east-1.amazonaws.com",
-          port: 2465,
-          tls: true,
-          auth: {
-            username: username,
-            password: password,
-          },
+      connection: {
+        hostname: "email-smtp.us-east-1.amazonaws.com",
+        port: 2465,
+        tls: true,
+        auth: {
+          username: username,
+          password: password,
         },
-      });
-      
-    // console.debug(username)
-    // console.debug(password)
-    console.debug('connected')
-      await client.send({
-        from: "ayeoh.dev@gmail.com",
-        to: "ayeoh.dev@gmail.com",
-        subject: "Ayeoh.Dev Contact Submission",
-        content: fullName + '<br/>'+email+ '<br/>'+message,
-        html: "<p>...</p>",
-      });
-      console.debug('sent')
-      
-      await client.close();
-      console.debug('closed')
-    
-    // const client = new SmtpClient();
-    // const env = await load();
-    // const username = env["USERNAME"];
-    // const password = env["PASSWORD"];
+      },
+    });
 
-    // console.debug('got creds')
-    // await client.connectTLS({
-    //   hostname: "email-smtp.us-east-1.amazonaws.com",
-    //   port: 2465,
-    //   username: username,
-    //   password: password,
-    // });
-    // console.debug('connected')
+    await client.send({
+      from: "ayeoh.dev@gmail.com",
+      to: "ayeoh.dev@gmail.com",
+      subject: "Ayeoh.Dev Contact Submission",
+      content: fullName + "<br/>" + email + "<br/>" + message,
+      html: `<p> ${fullName} <br/>${email} <br/> ${message}</p>`,
+    });
+    console.debug("sent");
 
-    // await client.send({
-    //   from: "ayeoh.dev@gmail.com", // Your Email address
-    //   to: "ayeoh.dev@gmail.com", // Email address of the destination
-    //   subject: "Ayeoh.Dev Contact Submission",
-    //   content: fullName + '<br/>'+email+ '<br/>'+message,
-    // });
+    await client.close();
+    console.debug("closed");
 
-    // console.debug('sent')
-
-    // await client.close();
-    // console.debug('closed')
-
-    const headers = new Headers(req.headers);
-    headers.set("location","/");
     return new Response(null, {
       status: 302,
       headers,
